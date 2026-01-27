cfcb60831b8962e6295c12f398620f81867e580c
Marcos Rica <66302998+Ygramul22@users.noreply.github.com>
2026-01-27 18:17:22 +0100

    Updated the form page to use BaseCard structure

--- frontend/src/components/Form.vue
+++ frontend/src/components/Form.vue
@@ -9,6 +9,7 @@
     import BaseRadioGroup from './BaseComponents/BaseRadioGroup.vue';
     import Header from './BuildingBlocks/Header.vue';
 import BasePage from './BuildingBlocks/BasePage.vue';
+import BaseCard from './BaseComponents/BaseCard.vue';
 
     //Object needed to fulfill the petition
     const petitionMaker:PetitionMaker = new PetitionMaker();
@@ -60,103 +61,86 @@ import BasePage from './BuildingBlocks/BasePage.vue';
                 @close="showAlert = false"
             />
 
-        <div class="FormWrapper">
-            <div class="FormHeader">
+            <BaseCard customClass="FormHeader">
                 <div class="FormIcon">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"></path>
                     </svg>
                 </div>
-
+                
                 <h2 class="FormHeaderText"> <b> Crear una solicitud </b></h2>
-            </div>
+            </BaseCard>
 
-            <form class="FormDiv"  @submit.prevent="handleSubmit">
-                <div class="TypeSelection">
-                    <p> <b> Indique el tipo de solicitud </b> </p>
-                    <BaseRadioGroup
-                        v-model="documentType"
-                        name="DocumentType"
-                        :options="[
-                            { value: 'Idea', label: 'Idea' },
-                            { value: 'Complaint', label: 'Queja' },
-                            { value: 'Suggestion', label: 'Sugerencia' }
-                        ]"
-                        custom-class="MultiSelect_Type"
-                        gap="5px"
-                    />
-                </div>
+            <BaseCard customClass="FormDiv">
+                <form class="FormContent"  @submit.prevent="handleSubmit">
+                    <div class="TypeSelection">
+                        <p> <b> Indique el tipo de solicitud </b> </p>
+                        <BaseRadioGroup
+                            v-model="documentType"
+                            name="DocumentType"
+                            :options="[
+                                { value: 'Idea', label: 'Idea' },
+                                { value: 'Complaint', label: 'Queja' },
+                                { value: 'Suggestion', label: 'Sugerencia' }
+                            ]"
+                            custom-class="MultiSelect_Type"
+                            gap="5px"
+                        />
+                    </div>
 
-                <div class="Department">
-                    <p> <b> Indique la subdelegación a la que se quiere dirigir </b> </p>
+                    <div class="Department">
+                        <p> <b> Indique la subdelegación a la que se quiere dirigir </b> </p>
 
-                    <BaseSelect
-                      v-model="department"
-                      label=""
-                      placeholder="Selecciona una opción"
-                      :options="[
-                        { value: 'General', label: 'General' },
-                        { value: 'AtencionEstudiante', label: 'Subdelegación de Ayuda y Servicios para el Estudiante' },
-                        { value: 'Comunicacion', label: 'Subdelegación de Comunicación' },
-                        { value: 'Calidad', label: 'Subdelegación de Mediación y Calidad Académica' },
-                        { value: 'TIC', label: 'Subdelegación de Estrategia y Desarrollo Tecnológico' },
-                        { value: 'Eventos', label: 'Subdelegación de Eventos' },
-                        { value: 'Igualdad', label: 'Subdelegación de Bienestar e Igualdad Social' }
-                      ]"
-                    />
-                </div>
+                        <BaseSelect
+                          v-model="department"
+                          label=""
+                          placeholder="Selecciona una opción"
+                          :options="[
+                            { value: 'General', label: 'General' },
+                            { value: 'AtencionEstudiante', label: 'Subdelegación de Ayuda y Servicios para el Estudiante' },
+                            { value: 'Comunicacion', label: 'Subdelegación de Comunicación' },
+                            { value: 'Calidad', label: 'Subdelegación de Mediación y Calidad Académica' },
+                            { value: 'TIC', label: 'Subdelegación de Estrategia y Desarrollo Tecnológico' },
+                            { value: 'Eventos', label: 'Subdelegación de Eventos' },
+                            { value: 'Igualdad', label: 'Subdelegación de Bienestar e Igualdad Social' }
+                          ]"
+                        />
+                    </div>
 
-                <div class="Description">
-                    <p> <b> Describa su solicitud </b> </p>
-                    <BaseInput
-                      v-model="subject"
-                      name="Description"
-                      placeholder="Asunto"
-                      custom-class="Subject"
-                    />
-                    <BaseTextArea
-                      v-model="description"
-                      name="Description"
-                      placeholder="Describa su solicitud..."
-                      customClass="Explanation"
-                    />
-                </div>
+                    <div class="Description">
+                        <p> <b> Describa su solicitud </b> </p>
+                        <BaseInput
+                          v-model="subject"
+                          name="Description"
+                          placeholder="Asunto"
+                          custom-class="Subject"
+                        />
+                        <BaseTextArea
+                          v-model="description"
+                          name="Description"
+                          placeholder="Describa su solicitud..."
+                          customClass="Explanation"
+                        />
+                    </div>
 
-                <div class="SubmitDiv">
-                    <BaseButton type="submit" variant="primary" customClass="SubmitButton">
-                        <b>Enviar solicitud</b>
-                    </BaseButton>
-                </div>
-            </form>
-        </div>
+                    <div class="SubmitDiv">
+                        <BaseButton type="submit" variant="primary" customClass="SubmitButton">
+                            <b>Enviar solicitud</b>
+                        </BaseButton>
+                    </div>
+                </form>
+            </BaseCard>
     </BasePage>
 </template>
 
 <style scoped>
-    .FormWrapper {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        width: 100%;
-    }
-
     .FormHeader {
-        /* Width and height */
-        min-height: auto;
-        width: 100%;
-
         /* Overall structure */
         display:flex;
         flex-direction: row;
         align-items: center;
         justify-content: center;
         
-        /* Colors */
-        background-color: var(--panel-background);
-        box-shadow:
-            0 1px 2px rgba(0, 0, 0, 0.921),
-            0 2px 6px rgba(0, 0, 0, 0.284);
-        border-radius: 10px;
 
         /* Margins and padding */
         margin-bottom: 10px;
@@ -177,24 +161,6 @@ import BasePage from './BuildingBlocks/BasePage.vue';
     }
 
     .FormDiv {
-        /* Overall structure */
-        display:flex;
-        flex: 1;
-        flex-direction: column;
-        align-items: center;
-        justify-content: top;
-        
-        /* Width and height */
-        width: 100%;
-        min-height: auto;
-
-        /* Colors */
-        background-color: var(--panel-background);
-        box-shadow:
-            0 1px 2px rgba(0, 0, 0, 0.921),
-            0 2px 6px rgba(0, 0, 0, 0.284);
-        border-radius: 10px;
-
         /* Margins and padding */
         margin-bottom: 10px;
         padding-top: 20px;
@@ -273,5 +239,14 @@ import BasePage from './BuildingBlocks/BasePage.vue';
 
     .SubmitButton {
         margin-bottom: 20px;
+    }
+
+    .FormContent {
+        width: 100%;
+        
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
 </style>
