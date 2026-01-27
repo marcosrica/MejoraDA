7b2ffb2a83a5401ca87081b209635395d945db68
Marcos Rica <66302998+Ygramul22@users.noreply.github.com>
2026-01-27 22:09:08 +0100

    Changed slightly the index page, so that it has a first iteration of the text, and that it fixes the buttons

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -81,12 +81,16 @@
         <p class="DescriptionText">
           Desde la delegación de alumnos, siempre estamos atentos a todo lo que ocurra en nuestra escuela, y estamos buscando constantemente formas de mejorar la ETSISI para todos. 
         </p>
+        <p class="DescriptionText">
+          Por ello, os hemos habilitado mejoraDA, una plataforma completamente anónima en la que nos podeis contar vuestra visión de la escuela, todo aquello que os gustaría que ocurriese, y las cosas que desearíais que cambiasen. 
+        </p>
     </BaseCard>
 
     <!-- Create a form -->
     <BaseCard custom-class="BaseContainer">
       <p class="HelpHeader"><b>¿Tienes algo que contarnos?</b></p>
-      <p class="DescriptionText"><b>No dudes en rellenar una nueva solicitud, ¡es totalmente anónima! Tardarás menos de cinco minutos en hacerla, y nos será muy útil para mejorar la experiencia en la ETSISI.</b></p>
+      <p class="DescriptionText">No dudes en rellenar una nueva solicitud, <b>¡es totalmente anónima!</b> </p>
+      <p class="DescriptionText">Tardarás <b>menos de cinco minutos</b> en hacerla, y nos será muy útil para <b>mejorar la experiencia de todos</b> en la escuela.</p>
       <BaseButton  
        @click="fillFormButtonClicked"
        variant="primary"
@@ -134,7 +138,7 @@
   }
 
   .FillFormButton {
-    width: 20%;
+    min-width: 20%;
   }
 
   .Index_Numbers {
