c22de97f95b18bda3a74884b514926cec645b616
Marcos Rica <66302998+Ygramul22@users.noreply.github.com>
2026-01-30 12:35:04 +0100

    Simplified the BasePage component

--- frontend/src/components/BuildingBlocks/BasePage.vue
+++ frontend/src/components/BuildingBlocks/BasePage.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import BaseMenu from '../BaseComponents/BaseMenu.vue';
-import Header from './Header.vue';
+  import { ref } from 'vue';
+  import BaseMenu from '../BaseComponents/BaseMenu.vue';
+  import Header from './Header.vue';
 
-const privilegedUser = ref(true); // This should be set based on actual user privileges
+  const privilegedUser = ref(true); // This should be set based on actual user privileges
 </script>
 
 <template>
@@ -23,57 +23,57 @@ const privilegedUser = ref(true); // This should be set based on actual user pri
 </template>
 
 <style scoped>
-    .Home_background {
-        height: 100dvh;
-        width: 100dvw;
+  .Home_background {
+    height: 100dvh;
+    width: 100dvw;
 
-        display: flex;
-        flex-direction: column;
+    display: flex;
+    flex-direction: column;
 
-        background-color: var(--page-background);
+    background-color: var(--page-background);
+  }
+
+  .Home_content_wrapper {
+    box-sizing: border-box;
+    width: 100%;
+    height: auto;
+    margin-top: 10dvh;
+    box-sizing: border-box;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+
+    overflow-y: auto;
+  }
+
+  .Home_content {
+    /* Width and height */
+    box-sizing: border-box;
+
+    height: auto;
+    box-sizing: border-box;
+
+    /* Margins */
+    padding-top: 20px;
+    padding-bottom: 50px;
+    padding-left: 5px;
+    padding-right: 5px;
+
+    @media (orientation: landscape) {
+      @media (max-width: 1000px) {
+        width: 90%;
+      }
+      @media (max-width: 1450px) {
+        width: 80%;
+      }
+      @media (min-width: 1451px) {
+        width: 60%;
+      }
     }
-
-    .Home_content_wrapper {
-        box-sizing: border-box;
-        width: 100%;
-        height: auto;
-        margin-top: 10dvh;
-        box-sizing: border-box;
-
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-
-        overflow-y: auto;
+    @media (orientation: portrait) {
+      width: 95%;
     }
-
-    .Home_content {
-        /* Width and height */
-        box-sizing: border-box;
-
-        height: auto;
-        box-sizing: border-box;
-
-        /* Margins */
-        padding-top: 20px;
-        padding-bottom: 50px;
-        padding-left: 5px;
-        padding-right: 5px;
-
-        @media (orientation: landscape) {
-          @media (max-width: 1000px) {
-            width: 90%;
-          }
-          @media (max-width: 1450px) {
-            width: 80%;
-          }
-          @media (min-width: 1451px) {
-            width: 60%;
-          }
-        }
-        @media (orientation: portrait) {
-          width: 95%;
-        }
-    }
+  }
 </style>
