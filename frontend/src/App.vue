938800b45f682b811cb47df984aae0359a341d42
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-09-10 11:35:53 +0200

    Fixed indentation on App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import startCheck from './Utilities/TokenExpiringChecker';
-
-onMounted(() => {
-  startCheck();
-})
+    import { onMounted } from 'vue';
+    import startCheck from './Utilities/TokenExpiringChecker';
+    
+    onMounted(() => {
+        startCheck();
+    });
 </script>
 
 <template>
-  <router-view />
+    <router-view />
 </template>
 
 <style>
-html, body {
-  margin: 0;
-  padding: 0;
-  font-family:
-    system-ui,
-    -apple-system,
-    BlinkMacSystemFont,
-    "Segoe UI",
-    Roboto,
-    Helvetica,
-    Arial,
-    sans-serif;
-}
+    html, body {
+        margin: 0;
+        padding: 0;
+        font-family:
+            system-ui,
+            -apple-system,
+            BlinkMacSystemFont,
+            "Segoe UI",
+            Roboto,
+            Helvetica,
+            Arial,
+            sans-serif;
+    }
 </style>
