78ff4f659cd1046df972a7b89e4a9d7de3cb2c9a
Marcos Rica <marcos.rica.sanchez@gmail.com>
2026-09-10 11:39:02 +0200

    Fixed indentation and added a check whenever a user is logged in to see if it is also the system's admin

--- frontend/src/components/BuildingBlocks/BasePage.vue
+++ frontend/src/components/BuildingBlocks/BasePage.vue
@@ -1,122 +1,126 @@
 <script setup lang="ts">
-  import { onMounted, ref } from 'vue';
-  import Header from './Header.vue';
-  import PetitionMaker from '../../Utilities/PetitionMaker';
-  import BaseNotAuth from '../BaseComponents/BaseNotAuth.vue';
+    import { onMounted, ref } from 'vue';
+    import Header from './Header.vue';
+    import PetitionMaker from '../../Utilities/PetitionMaker';
+    import BaseNotAuth from '../BaseComponents/BaseNotAuth.vue';
 
-  const props = defineProps<{
-    showContent: boolean
-  }>()
+    const props = defineProps<{
+        showContent: boolean
+    }>()
 
-  const petitionMaker:PetitionMaker = new PetitionMaker();
+    const petitionMaker:PetitionMaker = new PetitionMaker();
 
-  //Tracks wether the user is privileged, to know if it should show the menu
-  const allowedUser = ref(false); 
-  const showHamburgerMenu = ref(false);
+    //Tracks wether the user is privileged, to know if it should show the menu
+    const allowedUser = ref(false); 
+    const showHamburgerMenu = ref(false);
 
-  const showHideHamburgerMenu = ():void => {
-    showHamburgerMenu.value = !showHamburgerMenu.value;
-  }
+    const showHideHamburgerMenu = ():void => {
+        showHamburgerMenu.value = !showHamburgerMenu.value;
+    }
 
-  const getPrivileged = async () => {
-    const response = await petitionMaker.makeGetPetition("/api/auth/amIPrivileged");
+    const getPrivileged = async () => {
+        const response = await petitionMaker.makeGetPetition("/api/auth/amIPrivileged");
+        allowedUser.value = response.status == 200;
 
-    allowedUser.value = response.status == 200;
-  };
+        if (allowedUser.value) {
+            const adminResponse = await petitionMaker.makeGetPetition("/api/auth/amIAdmin");
+            console.log(adminResponse);
+        }
+    };
 
-  onMounted(async () => {
-    await getPrivileged();
-    console.log(allowedUser);
-  });
+    onMounted(async () => {
+        await getPrivileged();
+        console.log(allowedUser);
+    });
 </script>
 
 <template>
-  <div class="Home_background">
-    <Header :isAdmin="allowedUser" :showHamburgerMenu="showHamburgerMenu" :enableHamburguerMenu="showHideHamburgerMenu"/>
+    <div class="Home_background">
+        <Header :isAdmin="allowedUser" :showHamburgerMenu="showHamburgerMenu" :enableHamburguerMenu="showHideHamburgerMenu"/>
 
-    <div class="Home_content_wrapper">
-      <div class="HamburgerMenuBlackout" v-if="showHamburgerMenu" v-on:click="showHideHamburgerMenu" />
+        <div class="Home_content_wrapper">
+            <div class="HamburgerMenuBlackout" v-if="showHamburgerMenu" v-on:click="showHideHamburgerMenu" />
 
-      <!-- Content area -->
-      <div class="Home_content">
-        <slot v-if="props.showContent" />
-        <BaseNotAuth v-else />
-      </div>
+            <!-- Content area -->
+            <div class="Home_content">
+                <slot v-if="props.showContent" />
+                <BaseNotAuth v-else />
+            </div>
 
-      <!-- Toggle menu that sits on top of the content -->
-      
-      <!--
-      <BaseMenu :visible="allowedUser" :privileged="allowedUser" :admin="true"/>
-      
-      
-      <LoginMenuFloating :visible="!loggedIn"></LoginMenuFloating>
-      -->
+            <!-- Toggle menu that sits on top of the content -->
+            
+            <!--
+            <BaseMenu :visible="allowedUser" :privileged="allowedUser" :admin="true"/>
+            
+            
+            <LoginMenuFloating :visible="!loggedIn"></LoginMenuFloating>
+            -->
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
-  .Home_background {
-    height: 100dvh;
-    width: 100dvw;
-
-    display: flex;
-    flex-direction: column;
-
-    background-color: var(--page-background);
-  }
-
-  .Home_content_wrapper {
-    box-sizing: border-box;
-    width: 100%;
-    margin-top: 10dvh;
-    box-sizing: border-box;
-    height: calc(100dvh - 10dvh);
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-start;
-
-    overflow-y: auto;
-  }
-
-  .Home_content {
-    /* Width and height */
-    box-sizing: border-box;
-    height: auto;
-
-    /* Margins */
-    padding-top: 20px;
-    padding-left: 5px;
-    padding-right: 5px;
-    padding-bottom: 80px;
-
-    @media (orientation: landscape) {
-      @media (max-width: 1000px) {
-        width: 90%;
-      }
-      @media (max-width: 1450px) {
-        width: 80%;
-      }
-      @media (min-width: 1451px) {
-        width: 60%;
-      }
+    .Home_background {
+        height: 100dvh;
+        width: 100dvw;
+        
+        display: flex;
+        flex-direction: column;
+        
+        background-color: var(--page-background);
     }
-    @media (orientation: portrait) {
-      width: 95%;
+
+    .Home_content_wrapper {
+        box-sizing: border-box;
+        width: 100%;
+        margin-top: 10dvh;
+        box-sizing: border-box;
+        height: calc(100dvh - 10dvh);
+        
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+        
+        overflow-y: auto;
     }
-  }
 
-  .HamburgerMenuBlackout {
-    width: 100%;
-    height: 100%;
+    .Home_content {
+        /* Width and height */
+        box-sizing: border-box;
+        height: auto;
+        
+        /* Margins */
+        padding-top: 20px;
+        padding-left: 5px;
+        padding-right: 5px;
+        padding-bottom: 80px;
+        
+        @media (orientation: landscape) {
+            @media (max-width: 1000px) {
+                width: 90%;
+            }
+            @media (max-width: 1450px) {
+                width: 80%;
+            }
+            @media (min-width: 1451px) {
+                width: 60%;
+            }
+        }
+        @media (orientation: portrait) {
+            width: 95%;
+        }
+    }
 
-    position: fixed;
-    bottom: 0px;
-
-    background-color: rgba(0, 0, 0, 0.663);
-
-    z-index: 10;
-  }
+    .HamburgerMenuBlackout {
+        width: 100%;
+        height: 100%;
+        
+        position: fixed;
+        bottom: 0px;
+        
+        background-color: rgba(0, 0, 0, 0.663);
+        
+        z-index: 10;
+    }
 </style>
